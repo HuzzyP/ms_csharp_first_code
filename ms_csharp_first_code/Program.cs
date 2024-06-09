60fceb79fe09c5359b88c7d9ec14e826a10bdc78
HuzzyP <huzzy101@live.co.uk>
2024-06-09 11:59:54 +0100

    creates c# repo

---
+++ ms_csharp_first_code/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ms_csharp_first_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
